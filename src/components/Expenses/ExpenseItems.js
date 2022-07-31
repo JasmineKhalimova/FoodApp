ac72135ea905e6b3b487fc5945aee50c8c7055d8
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2022-07-31 14:29:02 +0100

    working with events and state

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,4 +1,6 @@
+import {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 function ExpenseItems(props){
     const expense = [
@@ -21,8 +23,16 @@ function ExpenseItems(props){
           date: new Date(2022, 8, 15),
         }
       ];
+      // filetrs 
+      const [selectedYear, setYear] = useState(2020);
+
+      const onChangeFilter = selectedYear => {
+        setYear(selectedYear);
+      }
+      
       return (
         <div className="expenses">
+          <ExpensesFilter select = {selectedYear} onChangeFilter={onChangeFilter}/>
           <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
           <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
           <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
